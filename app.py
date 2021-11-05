02a3e698c84b5ff46260ef597e7f8378a2dcb21e
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-11-05 17:44:57 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def predict():
     return render_template('index.html', prediction_text='CO2 Emission of the vehicle is :{}'.format(output))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
